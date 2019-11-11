ec488f3d323741fbdf1723e70b996967901c2fa6
parthberk <sh.prth@yahoo.com>
2019-11-11 15:08:59 -0800

    O(n) Time and O(n) Space using deque

---
+++ 938. Range Sum of BST/938. Range Sum of BST.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def rangeSumBST(self, root: TreeNode, L: int, R: int) -> int:
+        q = collections.deque()
+        total=0
+        q.appendleft(root)
+        node = None
+        while q:
+            node = q.pop()
+            if node.val >= L and node.val <= R:
+                total+=node.val
+            if node.left:
+                q.append(node.left)
+            if node.right:
+                q.append(node.right)
+        return total
+        
+        
