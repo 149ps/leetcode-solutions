e29720660d0deca7a176b28da0c812c8f6251c5d
parthberk <sh.prth@yahoo.com>
2019-11-17 18:49:48 -0800

    O(nlogn) time for sorting and O(n) space for using stack

--- 56. Merge Intervals/56. Merge Intervals.py
+++ 56. Merge Intervals/56. Merge Intervals.py
@@ -2,17 +2,16 @@ class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
         if not intervals:
             return []
-        def takefirst(li):
-            return li[0]
-        intervals.sort(key=takefirst)
+        if len(intervals) == 1:
+            return intervals
+        intervals.sort(key= lambda x: x[0])
         stack = collections.deque()
         stack.append(intervals[0])
         for i in range(1,len(intervals)):
-            if intervals[i][0] > stack[-1][1]:
-                stack.append(intervals[i])
-            elif intervals[i][0] <= stack[-1][1]:
+            if intervals[i][0] <= stack[-1][1]:
                 top = stack[-1]
-                top[1] = max(intervals[i][1],stack[-1][1])
                 stack.pop()
-                stack.append(top)
+                stack.append([top[0],max(top[1],intervals[i][1])])
+            else:
+                stack.append(intervals[i])
         return stack
