b9fad26cf52b1789c6b0ac0d33fb4956aed16933
parthberk <sh.prth@yahoo.com>
2019-11-24 23:52:18 -0800

    O(n^2) time and O(n) space

--- 338. Counting Bits/338. Counting Bits.py
+++ 338. Counting Bits/338. Counting Bits.py
@@ -5,12 +5,7 @@ class Solution(object):
         :rtype: List[int]
         """
         def count_ones(n):
-            bin_n = bin(n).replace("0b","")
-            count = 0 
-            for char in str(bin_n):
-                if char == '1':
-                    count += 1
-            return count
+            return bin(n).replace("0b","").count('1')
         final_arr = []
         for i in range(num+1):
             final_arr.append(count_ones(i))
