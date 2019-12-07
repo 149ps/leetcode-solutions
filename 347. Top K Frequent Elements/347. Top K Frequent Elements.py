5b5dbe5465d9c4eeed195461e5315bb5cc00ca98
parthberk <sh.prth@yahoo.com>
2019-12-06 23:09:42 -0800

    O(n) time and O(n) space for using Hashtable

--- 347. Top K Frequent Elements/347. Top K Frequent Elements.py
+++ 347. Top K Frequent Elements/347. Top K Frequent Elements.py
@@ -1,4 +1,3 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        hmap = collections.Counter(nums).most_common(k)
-        return [arr[0] for arr in hmap]
+        return [arr[0] for arr in collections.Counter(nums).most_common(k)]
