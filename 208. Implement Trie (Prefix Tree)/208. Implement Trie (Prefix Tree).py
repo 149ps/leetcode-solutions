c6920a02352d5101a7219d73694e967ab9e58a82
parthberk <sh.prth@yahoo.com>
2019-12-11 22:15:40 -0800

    O(m) time for search add and startswith where m is the length of the word and O(m) space for creating a dictionary

---
+++ 208. Implement Trie (Prefix Tree)/208. Implement Trie (Prefix Tree).py
@@ -0,0 +1,49 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.end = False
+        
+class Trie:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.root = TrieNode()
+        
+        
+
+    def insert(self, word: str) -> None:
+        """
+        Inserts a word into the trie.
+        """
+        node = self.root
+        for w in word:
+            if w not in node.children:
+                node.children[w] = TrieNode()
+            node = node.children[w]
+        node.end = True
+        
+
+    def search(self, word: str) -> bool:
+        """
+        Returns if the word is in the trie.
+        """
+        node = self.root
+        for w in word:
+            if w not in node.children:
+                return False
+            node = node.children[w]
+        return node.end
+        
+
+    def startsWith(self, prefix: str) -> bool:
+        """
+        Returns if there is any word in the trie that starts with the given prefix.
+        """
+        node = self.root
+        for w in prefix:
+            if w not in node.children:
+                return False
+            node = node.children[w]
+        return True
