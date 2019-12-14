1b82bf9c09687cde9016bb8b76b857ffe05c9900
parthberk <sh.prth@yahoo.com>
2019-12-13 18:45:52 -0800

    O(n) time and O(n) space for using a trie

--- 211. Add and Search Word - Data structure design/211. Add and Search Word - Data structure design.py
+++ 211. Add and Search Word - Data structure design/211. Add and Search Word - Data structure design.py
@@ -1,7 +1,7 @@
 class TrieNode:
     def __init__(self):
         self.children = {}
-        self.end = True
+        self.end = False
         
 class WordDictionary:
 
@@ -11,17 +11,16 @@ class WordDictionary:
         """
         self.root = TrieNode()
         
-        
 
     def addWord(self, word: str) -> None:
         """
         Adds a word into the data structure.
         """
         node = self.root
-        for w in word:
-            if w not in node.children:
-                node.children[w] = TrieNode()
-            node = node.children[w]
+        for letter in word:
+            if letter not in node.children:
+                node.children[letter] = TrieNode()
+            node = node.children[letter]
         node.end = True
         
 
@@ -30,18 +29,18 @@ class WordDictionary:
         Returns if the word is in the data structure. A word could contain the dot character '.' to represent any one letter.
         """
         node = self.root
-        for i,w in enumerate(word):
-            if w == '.':
+        for i,letter in enumerate(word):
+            if letter == '.':
                 for child in node.children:
                     temp_trie = WordDictionary()
                     temp_trie.root = node.children[child]
                     if temp_trie.search(word[i+1:]):
                         return True
-                return False            
-            elif w not in node.children:
+            elif letter not in node.children:
                 return False
-            node = node.children[w]
+            node = node.children[letter]
         return node.end
+
 
 # Your WordDictionary object will be instantiated and called as such:
 # obj = WordDictionary()
