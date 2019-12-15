4d5529e6b26780170108c969cd59166859a6cb54
parthberk <sh.prth@yahoo.com>
2019-12-14 23:55:16 -0800

    O(n) time and O(n) space using Trie

--- 648. Replace Words/648. Replace Words.py
+++ 648. Replace Words/648. Replace Words.py
@@ -2,33 +2,32 @@ class TrieNode:
     def __init__(self):
         self.children = {}
         self.end = False
+
 class Trie:
     def __init__(self):
         self.root = TrieNode()
         
-    def add(self,word):
+    def addWord(self,word):
         node = self.root
-        for w in word:
-            if w not in node.children:
-                node.children[w] = TrieNode()
-            node = node.children[w]
+        for letter in word:
+            if letter not in node.children:
+                node.children[letter] = TrieNode()
+            node = node.children[letter]
         node.end = True
-        
 class Solution:
     def replaceWords(self, dict: List[str], sentence: str) -> str:
         trie = Trie()
         for root in dict:
-            trie.add(root)
-        def replace(word):
+            trie.addWord(root)
+        def replaceRoot(word):
             node = trie.root
-            temp_s = ''
-            for w in word:
-                if w not in node.children:
+            temp_str = ''
+            for letter in word:
+                if letter not in node.children:
                     break
-                node = node.children[w]
-                temp_s += w
+                node = node.children[letter]
+                temp_str += letter
                 if node.end:
-                    return temp_s
+                    return temp_str
             return word
-        return ' '.join(map(replace,sentence.split()))
-        
+        return ' '.join(map(replaceRoot,sentence.split(' ')))
