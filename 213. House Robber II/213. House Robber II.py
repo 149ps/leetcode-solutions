da4b100220e952a096d401ccd184696479f46197
parthberk <sh.prth@yahoo.com>
2019-12-15 17:47:52 -0800

    O(n) time and O(1) space DP. Look for the corner case.

--- 213. House Robber II/213. House Robber II.py
+++ 213. House Robber II/213. House Robber II.py
@@ -1,12 +1,32 @@
+"""
+
+You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed. All houses at this place are arranged in a circle. That means the first house is the neighbor of the last one. Meanwhile, adjacent houses have security system connected and it will automatically contact the police if two adjacent houses were broken into on the same night.
+
+Given a list of non-negative integers representing the amount of money of each house, determine the maximum amount of money you can rob tonight without alerting the police.
+
+Example 1:
+
+Input: [2,3,2]
+Output: 3
+Explanation: You cannot rob house 1 (money = 2) and then rob house 3 (money = 2),
+             because they are adjacent houses.
+Example 2:
+
+Input: [1,2,3,1]
+Output: 4
+Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
+             Total amount you can rob = 1 + 3 = 4.
+
+"""
 class Solution:
     def rob(self, nums: List[int]) -> int:
         if len(nums) == 1:
             return nums[0]
-        def rob(nums):
-            cur_max,prev_max = 0,0
+        def rob_1(nums):
+            cur_sum, last_sum = 0,0
             for num in nums:
-                temp = cur_max
-                cur_max = max(cur_max,prev_max+num)
-                prev_max = temp
-            return cur_max
-        return max(rob(nums[1:]),rob(nums[:-1]))
+                temp = cur_sum
+                cur_sum = max(last_sum + num, cur_sum)
+                last_sum = temp
+            return cur_sum
+        return max(rob_1(nums[1:]),rob_1(nums[:-1]))
