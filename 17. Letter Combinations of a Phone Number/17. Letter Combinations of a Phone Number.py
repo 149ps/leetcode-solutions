2a4afd1cd4e15ffb432a1a623f1ff8a02b4d57c7
parthberk <sh.prth@yahoo.com>
2019-12-19 23:16:35 -0800

    O(3^N * 4^M) time  where M is length of digits which has 4 letters and N is the length of digits which has 3 lettersand O(3^N * 4^M) space

---
+++ 17. Letter Combinations of a Phone Number/17. Letter Combinations of a Phone Number.py
@@ -0,0 +1,22 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        phone = {'2': ['a', 'b', 'c'],
+                 '3': ['d', 'e', 'f'],
+                 '4': ['g', 'h', 'i'],
+                 '5': ['j', 'k', 'l'],
+                 '6': ['m', 'n', 'o'],
+                 '7': ['p', 'q', 'r', 's'],
+                 '8': ['t', 'u', 'v'],
+                 '9': ['w', 'x', 'y', 'z']}
+        def dfs(string, temp_digits):
+            if len(temp_digits) == 0:
+                combinations.append(string)
+                return
+            else:
+                for letter in phone[temp_digits[0]]:
+                    dfs(string+letter, temp_digits[1:])
+        if not digits:
+            return []
+        combinations = []
+        dfs('',digits)
+        return combinations
