6a8f9d260e77c07934497a50dd2096baa4a57036
parthberk <sh.prth@yahoo.com>
2019-12-20 18:23:59 -0800

    O(n) time and O(n) space using trie where n is all the characters in strings

--- 14. Longest Common Prefix/14. Longest Common Prefix.py
+++ 14. Longest Common Prefix/14. Longest Common Prefix.py
@@ -1,13 +1,13 @@
 class TrieNode:
     def __init__(self):
-        self.children = {}
         self.end = False
-        
+        self.children = {}
+
 class Trie:
     def __init__(self):
         self.root = TrieNode()
     
-    def addWord(self,word):
+    def add(self,word):
         node = self.root
         for letter in word:
             if letter not in node.children:
@@ -15,24 +15,22 @@ class Trie:
             node = node.children[letter]
         node.end = True
     
-    def search(self):
-        result = ''
+    def prefix(self):
         node = self.root
+        lcp = ''
         while node:
-            # return when reaches the end of word or when there are more than 1 branches
             if node.end or len(node.children) > 1:
-                return result
-            c = list(node.children)[0]
-            result += c
-            node = node.children[c]
-                    
+                return lcp
+            char = list(node.children)[0]
+            lcp += char
+            node = node.children[char]
+            
+        
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
         if not strs:
             return ""
-        if len(strs) == 1:
-            return strs[0]
         trie = Trie()
-        for string in strs:
-            trie.addWord(string)
-        return trie.search()
+        for word in strs:
+            trie.add(word)
+        return trie.prefix()
