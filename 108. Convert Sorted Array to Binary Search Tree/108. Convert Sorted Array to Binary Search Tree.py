41656d7bd09ed35ffef94e081a52838c899c6da5
parthberk <sh.prth@yahoo.com>
2019-12-24 14:48:43 -0800

    O(n) time and O(n) space to generate Tree nodes using recursion(Binary Search) DC

--- 108. Convert Sorted Array to Binary Search Tree/108. Convert Sorted Array to Binary Search Tree.py
+++ 108. Convert Sorted Array to Binary Search Tree/108. Convert Sorted Array to Binary Search Tree.py
@@ -1,3 +1,20 @@
+"""
+Given an array where elements are sorted in ascending order, convert it to a height balanced BST.
+
+For this problem, a height-balanced binary tree is defined as a binary tree in which the depth of the two subtrees of every node never differ by more than 1.
+
+Example:
+
+Given the sorted array: [-10,-3,0,5,9],
+
+One possible answer is: [0,-3,9,-10,null,5], which represents the following height balanced BST:
+
+      0
+     / \
+   -3   9
+   /   /
+ -10  5
+"""
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, x):
@@ -7,17 +24,17 @@
 
 class Solution:
     def sortedArrayToBST(self, nums: List[int]) -> TreeNode:
-        def BST(A,left,right):
-            if left>right:
+        def BST(arr,left,right):
+            if left> right:
                 return None
-            if left==right:
-                temp_node = TreeNode(A[left])
-                temp_node.left = None
-                temp_node.right = None
+            elif left == right:
+                node = TreeNode(arr[left])
+                node.right = None
+                node.left = None
             else:
-                mid = left + (right-left)//2
-                temp_node = TreeNode(A[mid])
-                temp_node.left = BST(A,left,mid-1)
-                temp_node.right = BST(A,mid+1,right)
-            return temp_node
+                mid = left + (right - left)//2
+                node = TreeNode(arr[mid])
+                node.left = BST(arr,left,mid-1)
+                node.right = BST(arr,mid+1,right)
+            return node
         return BST(nums,0,len(nums)-1)
