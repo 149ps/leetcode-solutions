8c8446ce18eaed02530506a6c1bbf4dde1dc7491
parthberk <sh.prth@yahoo.com>
2019-12-24 17:16:17 -0800

    O(n) time using Inorder traversal and O(n) space for using Tree Nodes. Bottom Up Approach

--- 109. Convert Sorted List to Binary Search Tree/109. Convert Sorted List to Binary Search Tree.py
+++ 109. Convert Sorted List to Binary Search Tree/109. Convert Sorted List to Binary Search Tree.py
@@ -11,22 +11,38 @@
 #         self.left = None
 #         self.right = None
 
+"""
+Given a singly linked list where elements are sorted in ascending order, convert it to a height balanced BST.
+
+For this problem, a height-balanced binary tree is defined as a binary tree in which the depth of the two subtrees of every node never differ by more than 1.
+
+Example:
+
+Given the sorted linked list: [-10,-3,0,5,9],
+
+One possible answer is: [0,-3,9,-10,null,5], which represents the following height balanced BST:
+
+      0
+     / \
+   -3   9
+   /   /
+ -10  5
+"""
 class Solution:
     def sortedListToBST(self, head: ListNode) -> TreeNode:
-        count = 0
-        temp = head
+        temp,count = head,0
         while temp:
             temp = temp.next
-            count+=1
-        def list_BST(start,end):
+            count += 1
+        def BST(start,end):
             nonlocal head
             if start > end:
                 return None
-            mid = (start + end)//2
-            left = list_BST(start,mid-1)
+            mid = start + (end - start)//2
+            left = BST(start,mid-1)
             root = TreeNode(head.val)
             head = head.next
             root.left = left
-            root.right = list_BST(mid+1,end)
+            root.right = BST(mid+1,end)
             return root
-        return list_BST(0,count-1)
+        return BST(0,count-1)
