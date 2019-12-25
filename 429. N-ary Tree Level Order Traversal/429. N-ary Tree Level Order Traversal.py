21707955e0a7ee8e4f191321f52e76ee8fb0f307
parthberk <sh.prth@yahoo.com>
2019-12-24 20:18:15 -0800

    O(n) time and O(n) space using deque Iterative

---
+++ 429. N-ary Tree Level Order Traversal/429. N-ary Tree Level Order Traversal.py
@@ -0,0 +1,48 @@
+"""
+Given an n-ary tree, return the level order traversal of its nodes' values.
+
+Nary-Tree input serialization is represented in their level order traversal, each group of children is separated by the null value (See examples).
+
+ 
+
+Example 1:
+
+
+
+Input: root = [1,null,3,2,4,null,5,6]
+Output: [[1],[3,2,4],[5,6]]
+Example 2:
+
+
+
+Input: root = [1,null,2,3,4,5,null,null,6,7,null,8,null,9,10,null,null,11,null,12,null,13,null,null,14]
+Output: [[1],[2,3,4,5],[6,7,8,9,10],[11,12,13],[14]]
+"""
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+class Solution:
+    def levelOrder(self, root: 'Node') -> List[List[int]]:
+        if not root:
+            return None
+        q = collections.deque()
+        final_list,temp = [],[]
+        node = None
+        q.append(root)
+        q.append(None)
+        while q:
+            node = q.popleft()
+            if node:
+                temp.append(node.val)
+                if node.children:
+                    q.extend(node.children)
+            else:
+                final_list.append(temp)
+                temp = []
+                if q:
+                    q.append(None)
+        return final_list
