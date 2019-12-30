391fb5824a0513e6bb9ff6137cf930e4550cf9ac
parthberk <sh.prth@yahoo.com>
2019-12-29 18:07:37 -0800

    O(n*m) time and O(n*m) space for memoization

---
+++ 718. Maximum Length of Repeated Subarray/718. Maximum Length of Repeated Subarray.py
@@ -0,0 +1,26 @@
+"""
+Given two integer arrays A and B, return the maximum length of an subarray that appears in both arrays.
+
+Example 1:
+
+Input:
+A: [1,2,3,2,1]
+B: [3,2,1,4,7]
+Output: 3
+Explanation: 
+The repeated subarray with maximum length is [3, 2, 1].
+ 
+
+Note:
+
+1 <= len(A), len(B) <= 1000
+0 <= A[i], B[i] < 100
+"""
+class Solution:
+    def findLength(self, A: List[int], B: List[int]) -> int:
+        memoization = [[0] * (len(B)+1) for i in range(len(A)+1)]
+        for i in range(len(A)-1,-1,-1):
+            for j in range(len(B)-1,-1,-1):
+                if A[i] == B[j]:
+                    memoization[i][j] = memoization[i+1][j+1]+1
+        return max(max(row) for row in memoization)
