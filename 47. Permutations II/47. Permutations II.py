021847664717911f3e07082f3ee8ab762c80acd1
parthberk <sh.prth@yahoo.com>
2020-01-03 00:01:41 -0800

    O(n^2) time and O(n) space for stack using backtrack

--- 47. Permutations II/47. Permutations II.py
+++ 47. Permutations II/47. Permutations II.py
@@ -1,2 +1,31 @@
 """
+Given a collection of numbers that might contain duplicates, return all possible unique permutations.
+
+Example:
+
+Input: [1,1,2]
+Output:
+[
+  [1,1,2],
+  [1,2,1],
+  [2,1,1]
+]
 """
+class Solution:
+    def permuteUnique(self, nums):
+        result = []
+        visited = [False] * len(nums)
+        nums.sort()
+        def backtrack(nums,path):
+            if len(nums) == len(path):
+                result.append(path)
+                return
+            for i in range(len(nums)):
+                if not visited[i]:
+                    if i > 0 and not visited[i-1] and nums[i-1] == nums[i]:
+                        continue
+                    visited[i] = True
+                    backtrack(nums,path+[nums[i]])
+                    visited[i] = False
+        backtrack(nums,[])
+        return result
