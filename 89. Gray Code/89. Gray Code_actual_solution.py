ae924b5b771e1fd0eef214ac60d5f95d711fab61
parthberk <sh.prth@yahoo.com>
2020-01-04 21:25:12 -0800

    O(n^2) time and O(n) space

--- 89. Gray Code/89. Gray Code_actual_solution.py
+++ 89. Gray Code/89. Gray Code_actual_solution.py
@@ -2,7 +2,5 @@ class Solution:
     def grayCode(self, n: int) -> List[int]:
         result = [0]
         for i in range(n):
-            temp = [x + pow(2,i) for x in reversed(result)]
-            result += temp
-            temp = []
+            result += [a + pow(2,i) for a in reversed(result)]
         return result
