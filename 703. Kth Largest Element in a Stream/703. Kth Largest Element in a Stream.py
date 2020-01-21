dd6b633004e8e68199ea11c71856ef2561eea3f6
parthberk <sh.prth@yahoo.com>
2020-01-20 19:17:54 -0800

    O(n) time and O(k) space using min heap

--- 703. Kth Largest Element in a Stream/703. Kth Largest Element in a Stream.py
+++ 703. Kth Largest Element in a Stream/703. Kth Largest Element in a Stream.py
@@ -29,8 +29,9 @@ class KthLargest:
     def add(self, val: int) -> int:
         if len(self.heap) < self.k:
             heapq.heappush(self.heap,val)
-        elif val > self.heap[0]:
-            heapq.heapreplace(self.heap,val)
+        else:
+            if val > self.heap[0]:
+                heapq.heapreplace(self.heap,val)
         return self.heap[0]
 
 
