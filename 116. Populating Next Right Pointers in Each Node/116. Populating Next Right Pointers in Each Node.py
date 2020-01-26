ff5537e6d44b24091431447413379531962b28a2
parthberk <sh.prth@yahoo.com>
2020-01-25 23:12:54 -0800

    O(n) time and O(1) space

---
+++ 116. Populating Next Right Pointers in Each Node/116. Populating Next Right Pointers in Each Node.py
@@ -0,0 +1,44 @@
+"""
+You are given a perfect binary tree where all leaves are on the same level, and every parent has two children. The binary tree has the following definition:
+
+struct Node {
+  int val;
+  Node *left;
+  Node *right;
+  Node *next;
+}
+Populate each next pointer to point to its next right node. If there is no next right node, the next pointer should be set to NULL.
+
+Initially, all next pointers are set to NULL.
+
+ 
+
+Follow up:
+
+You may only use constant extra space.
+Recursive approach is fine, you may assume implicit stack space does not count as extra space for this problem.
+"""
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val, left, right, next):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+class Solution:
+    def connect(self, root: 'Node') -> 'Node':
+        if not root:
+            return None
+        if root.left:
+            root.left.next = root.right
+        if root.right:
+            if root.next:
+                root.right.next = root.next.left
+            else:
+                root.right.next = None
+        self.connect(root.left)
+        self.connect(root.right)
+        return root
+        
