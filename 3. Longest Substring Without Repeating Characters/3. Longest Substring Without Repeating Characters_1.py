3bfe736044767b4e5b1ba987463564e81fedec09
parthberk <sh.prth@yahoo.com>
2020-01-25 16:15:51 -0800

    O(n) time and O(n) space using hashtable.

--- 3. Longest Substring Without Repeating Characters/3. Longest Substring Without Repeating Characters_1.py
+++ 3. Longest Substring Without Repeating Characters/3. Longest Substring Without Repeating Characters_1.py
@@ -4,6 +4,6 @@ class Solution:
         for i,char in enumerate(s):
             if hmap.get(char) or hmap.get(char) == 0:
                 result = max(result,i-start)
-                start = max(start,hmap[char]+1)
+                start = max(start,hmap[char]+1) # why max? for string abba ans would be 3 instead of 2.
             hmap[char] = i
-        return max(result,len(s)-start)
+        return max(result,len(s)-start) # len(s) - start   if all the characters are distinct.
