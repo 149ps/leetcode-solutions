6cb7e9d13d0ec265658ff1bf3dc0e5233f13143d
parthberk <sh.prth@yahoo.com>
2020-01-27 12:21:39 -0800

    O(n^2) time and O(n) space using Hashtable.

---
+++ 438. Find All Anagrams in a String/438. Find All Anagrams in a String.py
@@ -0,0 +1,51 @@
+"""
+Given a string s and a non-empty string p, find all the start indices of p's anagrams in s.
+
+Strings consists of lowercase English letters only and the length of both strings s and p will not be larger than 20,100.
+
+The order of output does not matter.
+
+Example 1:
+
+Input:
+s: "cbaebabacd" p: "abc"
+
+Output:
+[0, 6]
+
+Explanation:
+The substring with start index = 0 is "cba", which is an anagram of "abc".
+The substring with start index = 6 is "bac", which is an anagram of "abc".
+Example 2:
+
+Input:
+s: "abab" p: "ab"
+
+Output:
+[0, 1, 2]
+
+Explanation:
+The substring with start index = 0 is "ab", which is an anagram of "ab".
+The substring with start index = 1 is "ba", which is an anagram of "ab".
+The substring with start index = 2 is "ab", which is an anagram of "ab".
+"""
+class Solution:
+    def findAnagrams(self, s: str, p: str) -> List[int]:
+        result = []
+        slow,fast = 0,len(p)
+        hmap = collections.Counter(p)
+        hmap_temp = collections.Counter(s[slow:fast])
+        while fast <= len(s):
+            if hmap == hmap_temp:
+                result.append(slow)
+            hmap_temp[s[slow]] -= 1
+            if hmap_temp[s[slow]] == 0:
+                del hmap_temp[s[slow]]
+            if fast < len(s):
+                if hmap_temp.get(s[fast]) or hmap_temp.get(s[fast]) == 0:
+                    hmap_temp[s[fast]] += 1
+                else:
+                    hmap_temp[s[fast]] = 1
+            slow += 1
+            fast += 1
+        return result
