128e64d220faf857d85f0c17d7d7c2bb6e5ce387
parthberk <sh.prth@yahoo.com>
2020-01-27 19:46:26 -0800

    O(n^2) time and O(1) space using DFS.

--- 200. Number of Islands/200. Number of Islands.py
+++ 200. Number of Islands/200. Number of Islands.py
@@ -22,20 +22,18 @@ Output: 3
 """
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        if not grid:
-            return 0
         def dfs(i,j):
-            if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]) or grid[i][j]!='1':
+            if i >= len(grid) or i < 0 or j >= len(grid[0]) or j < 0 or grid[i][j] != '1':
                 return
             grid[i][j] = '#'
-            dfs(i+1,j)
             dfs(i-1,j)
-            dfs(i,j+1)
+            dfs(i+1,j)
             dfs(i,j-1)
+            dfs(i,j+1)
         count = 0
         for i in range(len(grid)):
             for j in range(len(grid[0])):
                 if grid[i][j] == '1':
                     dfs(i,j)
-                    count+=1
+                    count += 1
         return count
