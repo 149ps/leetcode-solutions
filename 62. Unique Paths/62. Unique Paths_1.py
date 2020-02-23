58e7918d8fe9ec663fc65ce2ac17b41a1c03ce6d
parthberk <sh.prth@yahoo.com>
2020-02-23 00:07:45 -0800

    O(mn) time and O(mn) space using Memoization.

---
+++ 62. Unique Paths/62. Unique Paths_1.py
@@ -0,0 +1,7 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        memo = [[1] * n for _ in range(m)]
+        for row in range(1,m):
+            for col in range(1,n):
+                memo[row][col] = memo[row-1][col] + memo[row][col-1]
+        return memo[m-1][n-1]
