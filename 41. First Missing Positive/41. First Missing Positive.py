5b3dfd604c0457af00727ca1e550fe6248adc8a2
parthberk <sh.prth@yahoo.com>
2020-02-26 23:56:21 -0800

    O(n) time and O(n) space using hashset.

---
+++ 41. First Missing Positive/41. First Missing Positive.py
@@ -0,0 +1,32 @@
+"""
+Given an unsorted integer array, find the smallest missing positive integer.
+
+Example 1:
+
+Input: [1,2,0]
+Output: 3
+Example 2:
+
+Input: [3,4,-1,1]
+Output: 2
+Example 3:
+
+Input: [7,8,9,11,12]
+Output: 1
+Note:
+
+Your algorithm should run in O(n) time and uses constant extra space.
+"""
+class Solution:
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        if not nums:
+            return 1
+        max_num = max(nums)
+        hset = set(nums)
+        if max_num > 0:
+            for i in range(1,max_num):
+                if i not in hset:
+                    return i
+            return max_num+1
+        else:
+            return 1
