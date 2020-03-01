ead30e24ddca2345f1731adba3e447ebc338b338
parthberk <sh.prth@yahoo.com>
2020-02-29 19:56:24 -0800

    O(nlogk) time and O(k) space using Min heap.

---
+++ 23. Merge k Sorted Lists/23. Merge k Sorted Lists.py
@@ -0,0 +1,35 @@
+"""
+Merge k sorted linked lists and return it as one sorted list. Analyze and describe its complexity.
+
+Example:
+
+Input:
+[
+  1->4->5,
+  1->3->4,
+  2->6
+]
+Output: 1->1->2->3->4->4->5->6
+"""
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def mergeKLists(self, lists: List[ListNode]) -> ListNode:
+        min_heap = []
+        heapq.heapify(min_heap)
+        head = cur = ListNode(0)
+        for l in lists:
+            if l:
+                heapq.heappush(min_heap,(l.val,id(l),l))
+        while min_heap:
+            val,nodeid,node = heapq.heappop(min_heap)
+            cur.next = ListNode(val)
+            cur = cur.next
+            node = node.next
+            if node:
+                heapq.heappush(min_heap,(node.val,id(node),node))
+        return head.next
