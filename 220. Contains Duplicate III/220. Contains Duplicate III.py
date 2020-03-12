7ad63b354d7b0a91b1772fcbe9cc7f38f7dfb9a4
parthberk <sh.prth@yahoo.com>
2020-03-12 05:33:21 -0700

    O(n^2) time and O(n) using HashTable.

--- 220. Contains Duplicate III/220. Contains Duplicate III.py
+++ 220. Contains Duplicate III/220. Contains Duplicate III.py
@@ -16,6 +16,8 @@ Output: false
 """
 class Solution:
     def containsNearbyAlmostDuplicate(self, nums: List[int], k: int, t: int) -> bool:
+        if t == 0 and len(set(nums)) == len(nums):
+            return False
         hmap = {val:i for val,i in enumerate(nums)}
         for i in range(len(nums)):
             for j in range(i+1,len(nums)):
