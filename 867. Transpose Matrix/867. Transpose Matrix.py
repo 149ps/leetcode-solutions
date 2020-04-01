f69840115d9ce857789787246257f7f1c4af728f
parthberk <sh.prth@yahoo.com>
2020-03-31 22:48:46 -0700

    O(n*m) time and O(n*m) space using two for loops.

---
+++ 867. Transpose Matrix/867. Transpose Matrix.py
@@ -0,0 +1,31 @@
+"""
+Given a matrix A, return the transpose of A.
+
+The transpose of a matrix is the matrix flipped over it's main diagonal, switching the row and column indices of the matrix.
+
+
+
+ 
+
+Example 1:
+
+Input: [[1,2,3],[4,5,6],[7,8,9]]
+Output: [[1,4,7],[2,5,8],[3,6,9]]
+Example 2:
+
+Input: [[1,2,3],[4,5,6]]
+Output: [[1,4],[2,5],[3,6]]
+ 
+
+Note:
+
+1 <= A.length <= 1000
+1 <= A[0].length <= 1000
+"""
+class Solution:
+    def transpose(self, A: List[List[int]]) -> List[List[int]]:
+        final_matrix = [[0 for i in range(len(A))] for j in range(len(A[0]))]
+        for i in range(len(A[0])):
+            for j in range(len(A)):
+                final_matrix[i][j] = A[j][i]
+        return final_matrix
