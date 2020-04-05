2ee7cc9ab56ab9df2872a40b80531d4812533b9d
parthberk <sh.prth@yahoo.com>
2020-04-04 23:58:41 -0700

    O(n) time and O(1) space

--- 283. Move Zeroes/283. Move Zeroes_1.py
+++ 283. Move Zeroes/283. Move Zeroes_1.py
@@ -3,12 +3,11 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        count = 0
-        for i in range(len(nums)):
-            if nums[i]!=0:
-                nums[count] = nums[i]
-                count+=1
-        while count < len(nums):
-            nums[count]=0
-            count+=1
-        return nums
+        i,j = 0,0
+        while j < len(nums):
+            if nums[j] != 0:
+                nums[i] = nums[j]
+                i += 1
+            j += 1
+        for i in range(i,len(nums)):
+            nums[i] = 0
