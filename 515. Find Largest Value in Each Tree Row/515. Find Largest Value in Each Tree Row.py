d5d51a84e1ca86e9f28f0265972aae34e849884e
parthberk <sh.prth@yahoo.com>
2020-04-06 01:48:32 -0700

    O(n) time and O(n) space using Deque.

---
+++ 515. Find Largest Value in Each Tree Row/515. Find Largest Value in Each Tree Row.py
@@ -0,0 +1,40 @@
+"""
+You need to find the largest value in each row of a binary tree.
+
+Example:
+Input: 
+
+          1
+         / \
+        3   2
+       / \   \  
+      5   3   9 
+
+Output: [1, 3, 9]
+"""
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def largestValues(self, root: TreeNode) -> List[int]:
+        if not root:
+            return []
+        result = []
+        q = collections.deque()
+        q.append(root)
+        while q:
+            length = len(q)
+            temp = []
+            for i in range(length):
+                node = q.popleft()
+                temp.append(node.val)
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            result.append(max(temp))
+        return result
