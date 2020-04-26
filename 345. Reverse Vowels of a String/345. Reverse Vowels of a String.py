7bb7bdcb07f0e76b92d0dc62db165579be607d5e
parthberk <sh.prth@yahoo.com>
2020-04-25 20:28:55 -0700

    O(n) time and O(1) space using Two pointers.

--- 345. Reverse Vowels of a String/345. Reverse Vowels of a String.py
+++ 345. Reverse Vowels of a String/345. Reverse Vowels of a String.py
@@ -1,17 +1,29 @@
-class Solution(object):
-    def reverseVowels(self, s):
-        i,j = 0,len(s) - 1
+"""
+Write a function that takes a string as input and reverse only the vowels of a string.
+
+Example 1:
+
+Input: "hello"
+Output: "holle"
+Example 2:
+
+Input: "leetcode"
+Output: "leotcede"
+Note:
+The vowels does not include the letter "y".
+"""
+class Solution:
+    def reverseVowels(self, s: str) -> str:
         vowels = ['a','e','i','o','u','A','E','I','O','U']
+        i,j = 0,len(s) - 1
         list_s = list(s)
-        while i < j :
+        while i < j:
             if list_s[i] in vowels and list_s[j] in vowels:
-                temp = list_s[i]
-                list_s[i] = list_s[j]
-                list_s[j] = temp
+                list_s[i],list_s[j] = list_s[j],list_s[i]
                 i += 1
                 j -= 1
-            elif not list_s[i] in vowels:
-                i += 1
-            elif not list_s[j] in vowels:
+            elif list_s[i] in vowels and not list_s[j] in vowels:
                 j -= 1
+            else:
+                i += 1
         return ''.join(list_s)
