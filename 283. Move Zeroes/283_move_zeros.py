ce85ead90b627bbbbae9d2cb7b1a78f7debbea1d
parthberk <sh.prth@yahoo.com>
2020-05-05 01:00:53 -0700

    O(n) time and O(1) space.

--- 283. Move Zeroes/283_move_zeros.py
+++ 283. Move Zeroes/283_move_zeros.py
@@ -1,11 +1,27 @@
-def moveZeroes(nums):
-    for i in range(len(nums)):
-        if nums[i]==0:
-            nums+=[nums.pop(i)]
-    return nums
-            
-nums=[0,1,0,3,12]
-print(moveZeroes(nums))
+"""
+Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+Example:
+
+Input: [0,1,0,3,12]
+Output: [1,3,12,0,0]
+Note:
+
+You must do this in-place without making a copy of the array.
+Minimize the total number of operations.
+"""
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        count = 0
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[count] = nums[i]
+                count += 1
+        for i in range(count,len(nums)):
+            nums[i] = 0
         
             
     
