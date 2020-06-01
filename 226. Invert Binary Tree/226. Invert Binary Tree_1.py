08783276be7a52164e4ac2b597cdccc948ca735d
parthberk <sh.prth@yahoo.com>
2020-06-01 03:41:32 -0700

    O(n) time and O(n) space using Recursion.

--- 226. Invert Binary Tree/226. Invert Binary Tree_1.py
+++ 226. Invert Binary Tree/226. Invert Binary Tree_1.py
@@ -5,12 +5,17 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
     def invertTree(self, root: TreeNode) -> TreeNode:
         if not root:
             return None
-        right = self.invertTree(root.right)
-        left = self.invertTree(root.left)
-        root.left = right
-        root.right = left
+        root.left,root.right = root.right,root.left
+        self.invertTree(root.left)
+        self.invertTree(root.right)
         return root
