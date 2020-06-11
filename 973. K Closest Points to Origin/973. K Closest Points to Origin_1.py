c046eb501896c8996538320420f4ab64264104b7
parthberk <sh.prth@yahoo.com>
2020-06-10 22:32:42 -0700

    O(nlogk) time and O(n) space using min heap.

--- 973. K Closest Points to Origin/973. K Closest Points to Origin_1.py
+++ 973. K Closest Points to Origin/973. K Closest Points to Origin_1.py
@@ -1,9 +1,17 @@
 class Solution:
     def kClosest(self, points: List[List[int]], K: int) -> List[List[int]]:
-        def dist(x,y):
-            return (x**2+y**2)**(1/2)
-        minheap = []
-        heapq.heapify(minheap)
+        def distance(x,y):
+            return sqrt(x**2 + y**2)
+        result = []
+        min_heap = []
+        heapq.heapify(min_heap)
         for x,y in points:
-            heapq.heappush(minheap,(dist(x,y),[x,y]))
-        return [heapq.heappop(minheap)[1] for k in range(K)]
+            heapq.heappush(min_heap,(distance(x,y),(x,y)))
+        if len(min_heap) >= K:
+            while K:
+                result.append(heapq.heappop(min_heap)[1])
+                K -= 1
+        else:
+            while min_heap:
+                result.append(heapq.heappop(min_heap)[1])
+        return result
