ece8d791676c140a14f51babe706eaa72a396d18
parthberk <sh.prth@yahoo.com>
2020-06-15 20:01:41 -0700

    Solution added.

---
+++ 30 Days June Challange/Week 3/1. Search in a Binary Search Tree/solution_1.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def searchBST(self, root: TreeNode, val: int) -> TreeNode:
+        if not root:
+            return None
+        if root.val == val:
+            return root
+        left = self.searchBST(root.left, val)
+        right = self.searchBST(root.right, val)
+        if left:
+            return left
+        elif right:
+            return right
+        else:
+            return None
