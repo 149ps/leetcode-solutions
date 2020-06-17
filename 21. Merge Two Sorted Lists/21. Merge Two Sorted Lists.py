c4019129a2a3336708530b67459a1bbd5be6be74
parthberk <sh.prth@yahoo.com>
2020-06-16 19:57:38 -0700

    O(n) time and O(1) space using Iterative two pointers.

--- 21. Merge Two Sorted Lists/21. Merge Two Sorted Lists.py
+++ 21. Merge Two Sorted Lists/21. Merge Two Sorted Lists.py
@@ -1,20 +1,37 @@
+"""
+21. Merge Two Sorted Lists
+Easy
+
+4077
+
+581
+
+Add to List
+
+Share
+Merge two sorted linked lists and return it as a new sorted list. The new list should be made by splicing together the nodes of the first two lists.
+
+Example:
+
+Input: 1->2->4, 1->3->4
+Output: 1->1->2->3->4->4
+"""
 # Definition for singly-linked list.
 # class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
-
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
     def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
         prev = ListNode(-100)
-        head = prev
+        temp = prev
         while l1 and l2:
-            if l1.val <= l2.val:
-                prev.next = l1
-                l1 = l1.next
-            else:
-                prev.next = l2
+            if l1.val >= l2.val:
+                temp.next = l2
                 l2 = l2.next
-            prev = prev.next
-        prev.next = l1 if l1 else l2
-        return head.next
+            else:
+                temp.next = l1
+                l1 = l1.next
+            temp = temp.next
+        temp.next = l1 or l2
+        return prev.next
