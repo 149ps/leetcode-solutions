103f5d6bea61fd19382a70bde78b7ad9a0a3a4fb
parthberk <sh.prth@yahoo.com>
2020-06-17 20:35:34 -0700

    O(nlok) time and O(n) space using maxheap.

--- 215. Kth Largest Element in an Array/215. Kth Largest Element in an Array_1.py
+++ 215. Kth Largest Element in an Array/215. Kth Largest Element in an Array_1.py
@@ -1,8 +1,10 @@
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        heap = []
-        heapq.heapify(heap)
+        max_heap = []
+        heapq.heapify(max_heap)
         for num in nums:
-            heapq.heappush(heap,num)
-        return heapq.nlargest(k,heap)[-1]
-        
+            heapq.heappush(max_heap,(-1)*num)
+        while k > 1:
+            heapq.heappop(max_heap)
+            k -= 1
+        return (-1)*max_heap[0]
