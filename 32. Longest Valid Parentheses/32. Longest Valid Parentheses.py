8169b99825d9d7e215af0f070710e0958062751f
parthberk <sh.prth@yahoo.com>
2020-06-19 20:06:28 -0700

    O(n) time and O(n) space using stack.

---
+++ 32. Longest Valid Parentheses/32. Longest Valid Parentheses.py
@@ -0,0 +1,30 @@
+"""
+Given a string containing just the characters '(' and ')', find the length of the longest valid (well-formed) parentheses substring.
+
+Example 1:
+
+Input: "(()"
+Output: 2
+Explanation: The longest valid parentheses substring is "()"
+Example 2:
+
+Input: ")()())"
+Output: 4
+Explanation: The longest valid parentheses substring is "()()"
+"""
+
+
+class Solution:
+    def longestValidParentheses(self, s: str) -> int:
+        stack = [-1]
+        result = 0
+        for i in range(len(s)):
+            if s[i] == '(':
+                stack.append(i)
+            else:
+                stack.pop()
+                if not stack:
+                    stack.append(i)
+                else:
+                    result = max(result, i - stack[-1])
+        return result
