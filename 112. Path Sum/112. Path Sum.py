d51d00877518cb8c1fec3997384e480835e3093b
parthberk <sh.prth@yahoo.com>
2020-06-19 22:03:32 -0700

    O(n) time and O(n) space using BFS.

--- 112. Path Sum/112. Path Sum.py
+++ 112. Path Sum/112. Path Sum.py
@@ -33,3 +33,30 @@ class Solution:
         left = self.hasPathSum(root.left, sum - root.val)
         right = self.hasPathSum(root.right, sum - root.val)
         return left or right
+
+#Iterative version
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def hasPathSum(self, root: TreeNode, sum: int) -> bool:
+        if not root:
+            return False
+        q = collections.deque()
+        q.append((root,sum))
+        while q:
+            node,total = q.popleft()
+            #print(node.val,total)
+            if not node.left and not node.right:
+                if total == node.val:
+                    return True
+            if node.left:
+                q.append((node.left,total-node.val))
+            if node.right:
+                q.append((node.right,total-node.val))
+        return False
