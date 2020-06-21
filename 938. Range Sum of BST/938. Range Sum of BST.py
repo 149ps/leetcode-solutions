388aad6ceb6539e38a482ceda50ec3f457ea5a37
parthberk <sh.prth@yahoo.com>
2020-06-21 04:31:41 -0700

    O(n) time and O(n) space using deque.

--- 938. Range Sum of BST/938. Range Sum of BST.py
+++ 938. Range Sum of BST/938. Range Sum of BST.py
@@ -1,20 +1,42 @@
+"""
+Given the root node of a binary search tree, return the sum of values of all nodes with value between L and R (inclusive).
+
+The binary search tree is guaranteed to have unique values.
+
+
+
+Example 1:
+
+Input: root = [10,5,15,3,7,null,18], L = 7, R = 15
+Output: 32
+Example 2:
+
+Input: root = [10,5,15,3,7,13,18,1,null,6], L = 6, R = 10
+Output: 23
+
+
+Note:
+
+The number of nodes in the tree is at most 10000.
+The final answer is guaranteed to be less than 2^31.
+"""
 # Definition for a binary tree node.
 # class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
     def rangeSumBST(self, root: TreeNode, L: int, R: int) -> int:
+        if not root:
+            return 0
         q = collections.deque()
-        total=0
-        q.appendleft(root)
-        node = None
+        q.append(root)
+        total = 0
         while q:
-            node = q.pop()
-            if node.val >= L and node.val <= R:
-                total+=node.val
+            node = q.popleft()
+            if L <= node.val <= R:
+                total += node.val
             if node.left:
                 q.append(node.left)
             if node.right:
