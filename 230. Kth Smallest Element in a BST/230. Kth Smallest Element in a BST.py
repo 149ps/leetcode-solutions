1587a9e30c6d0d6c14e4a192b2490ed6b0afcbb1
parthberk <sh.prth@yahoo.com>
2020-06-20 17:03:06 -0700

    O(n) time and O(n) space using Iterative approach.

--- 230. Kth Smallest Element in a BST/230. Kth Smallest Element in a BST.py
+++ 230. Kth Smallest Element in a BST/230. Kth Smallest Element in a BST.py
@@ -1,24 +1,19 @@
 # Definition for a binary tree node.
 # class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
     def kthSmallest(self, root: TreeNode, k: int) -> int:
-        if not root:
-            return None
-        q = collections.deque()
-        q.appendleft(root)
-        arr = []
-        node = None
-        while q:
-            node = q.pop()
-            if node:
-                arr.append(node.val)
-            if node.left:
-                q.appendleft(node.left)
-            if node.right:
-                q.appendleft(node.right)
-        return sorted(arr)[k-1]
+        stack = []
+        cur = root
+        while cur or stack:
+            while cur:
+                stack.append(cur)
+                cur = cur.left
+            cur = stack.pop()
+            k -= 1
+            if not k:
+                return cur.val
+            cur = cur.right
