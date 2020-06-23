8af67ecb74fec06c234fbc0a78991e9a64890dc3
parthberk <sh.prth@yahoo.com>
2020-06-23 01:46:32 -0700

    Solution added.

---
+++ 30 Days June Challange/Week 4/2. Count Complete Tree Nodes/Count Complete Tree Nodes.py
@@ -0,0 +1,39 @@
+"""
+Given a complete binary tree, count the number of nodes.
+
+Note:
+
+Definition of a complete binary tree from Wikipedia:
+In a complete binary tree every level, except possibly the last, is completely filled, and all nodes in the last level are as far left as possible. It can have between 1 and 2h nodes inclusive at the last level h.
+
+Example:
+
+Input:
+    1
+   / \
+  2   3
+ / \  /
+4  5 6
+
+Output: 6
+"""
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def __init__(self):
+        self.count = 0
+
+    def countNodes(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+        else:
+            self.count += 1
+        self.countNodes(root.left)
+        self.countNodes(root.right)
+        return self.count
